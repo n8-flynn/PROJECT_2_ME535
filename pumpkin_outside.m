c0d3b45bf4ea78d0181e9e0905040a23918d1d26
Nate <nflynn4@wisc.edu>
2021-12-01 16:35:51 -0600

    Updated the control points and the function that generates the pumkin

--- pumpkin_outside.m
+++ pumpkin_outside.m
@@ -1,12 +1,14 @@
 %% Nathan Flynn - ME 535 - Project 1
 clc; clear; close all;
-for  theta = 0:pi/4:2*pi
-    N = 10; % number of intervals in u and v direction
+surface = 1; 
+DisplayCP = false;
+DisplaySurf = false; 
+N = 50; % Number of intervals in u and v direction (resolution)
+for  theta = 0:pi/4:(2*pi - pi/4)
 
     pumpkin_test_pts;
 
     quad_body = cat(3,[
-                       % body                      
                           1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16] , [
                           13 14 15 16; 17 18 19 20;21 22 23 24;25 26 27 28] , [
                           1 2 3 4; 5 29 30 31; 9 32 33 34; 13 35 36 37] , [ 
@@ -14,22 +16,22 @@ for  theta = 0:pi/4:2*pi
                           1 2 3 4; 8 44 45 46; 12 47 48 49; 16 50 51 52] , [ 
                           16 50 51 52; 20 53 54 55; 24 56 57 58; 25 26 27 28]);
 
-    DisplayCP = false;
 
-    quads = quad_body;  % tube - %Part 2               
-
-    % quads = quad_lid; %Part 6
+    quads = quad_body;  %           
     CtrlPt=zeros(4,4,3); % CP for one bicubic patch
     xyz=zeros((4+1),(4+1),3);
     bu=zeros(1, N);
     bv=zeros(1, N);
-    figure(1)
-   hold on;
-    axis equal;
-    % axis off;
-    ylabel('y');
-    zlabel('z');
-    xlabel('x');
+    
+    if DisplaySurf == true
+        figure(1)
+        hold on;
+        axis equal;
+        ylabel('y');
+        zlabel('z');
+        xlabel('x');
+    end
+    
     for k=1:size(quads,3)
             % get k-th patch's control points
             for i=1:4
@@ -91,10 +93,20 @@ for  theta = 0:pi/4:2*pi
             end
 
             % plot the patch
-            p = surf(xyz(:,:,1),xyz(:,:,2),xyz(:,:,3));
-            p.EdgeColor=k*[1,0,0]/32; %'green'; %0.5*[1 1 1 ]
-            p.FaceAlpha=1; %0.75;
-            p.FaceColor = [0.5 0.5 0.75];
+            
+            if DisplaySurf == true 
+                p = surf(xyz(:,:,1),xyz(:,:,2),xyz(:,:,3));
+                colormap default;
+                p.EdgeColor=k*[1,0,0]/32; %'green'; %0.5*[1 1 1 ]
+                p.FaceAlpha=1; %0.75;
+                p.FaceColor = [0.5 0.5 0.75];
+            end
+            
+            filename = sprintf('surf%d.stl',surface);
+            mm2in = 25.4;
+            surf2stl(filename,mm2in*xyz(:,:,1),mm2in*xyz(:,:,2),mm2in*xyz(:,:,3));
+            surface = surface + 1;
+            
             if DisplayCP == true
                 for i=1:4
                  plot3(CtrlPt(i,:,1),CtrlPt(i,:,2), CtrlPt(i,:,3),'-ro', 'linewidth',2,'MarkerFaceColor', 'r', 'MarkerSize',8);
@@ -104,4 +116,8 @@ for  theta = 0:pi/4:2*pi
                 end
             end
     end
+    if DisplaySurf  == true 
+        shading interp
+    end
 end
+
--- pumpkin_test_pts.m
+++ pumpkin_test_pts.m
@@ -1,63 +1,59 @@
-      d = 0.80; r = 3.343; rn1 = 2*pi/96; r0 = 4*pi/96; r1 = 6*pi/96; r2 = 10*pi/96; r3 = 14*pi/96; r4 = 18*pi/98; r5 = 20*pi/96; r6 = 22*pi/96; r7 = 24*pi/96; i = 1/2; verts = [ 0 0 3; % Start
-            0 0 3;
-            0 0 3;
-            0 0 3; % End
-            d*cos(r1) d*sin(r1) 4; % Start
-            cos(r2) sin(r2) 4;
-            cos(r3) sin(r3) 4;
-            d*cos(r4) d*sin(r4) 4; % End
-            d*r*cos(r1) d*r*sin(r1) 3.5; % Start
-            r*cos(r2) r*sin(r2) 3.5;
-            r*cos(r3) r*sin(r3) 3.5;
-            d*r*cos(r4) d*r*sin(r4) 3.5; % End
-            d*r*cos(r1) d*r*sin(r1) 2; % Start
-            r*cos(r2) r*sin(r2) 2;
-            r*cos(r3) r*sin(r3) 2;
-            d*r*cos(r4) d*r*sin(r4) 2; % End
-            d*r*cos(r1) d*r*sin(r1) 0.5; % Start
-            r*cos(r2) r*sin(r2) 0.5;
-            r*cos(r3) r*sin(r3) 0.5;
-            d*r*cos(r4) d*r*sin(r4) 0.5; % End
-            d*cos(r1) d*sin(r1) 0; % Start
-            cos(r2) sin(r2) 0;
-            cos(r3) sin(r3) 0;
-            d*cos(r4) d*sin(r4) 0; % 
-            0 0 1;
-            0 0 1;
-            0 0 1;
-            0 0 1; %
-            d*cos(r0) d*sin(r0) 4; %%%%%%%%%%%%%% 
-            cos(rn1) sin(rn1) 4;
-            cos(0) sin(0) 4;  % 1
-            d*r*cos(r0) d*r*sin(r0) 3.5;
-            r*cos(rn1) r*sin(rn1) 3.5;
-            r*cos(0) r*sin(0) 3.5; % 2
-            d*r*cos(r0) d*r*sin(r0) 2;
-            r*cos(rn1) r*sin(rn1) 2;
-            r*cos(0) r*sin(0) 2; % 3 
-            d*r*cos(r0) d*r*sin(r0) 0.5;
-            r*cos(rn1) r*sin(rn1) 0.5
-            r*cos(0) r*sin(0) 0.5; % 4
-            d*cos(r0) d*sin(r0) 0;
-            cos(rn1) sin(rn1) 0;
-            cos(0) sin(0) 0; %%%%%%%%%%
-            d*cos(r5) d*sin(r5) 4;
-            cos(r6) sin(r6) 4;
-            cos(r7) sin(r7) 4;  % 1
-            d*r*cos(r5) d*r*sin(r5) 3.5;
-            r*cos(r6) r*sin(r6) 3.5;
-            r*cos(r7) r*sin(r7) 3.5; % 2
-            d*r*cos(r5) d*r*sin(r5) 2;
-            r*cos(r6) r*sin(r6) 2;
-            r*cos(r7) r*sin(r7) 2; % 3 
-            d*r*cos(r5) d*r*sin(r5) 0.5;
-            r*cos(r6) r*sin(r6) 0.5
-            r*cos(r7) r*sin(r7) 0.5; % 4
-            d*cos(r5) d*sin(r5) 0;
-            cos(r6) sin(r6) 0;
-            cos(r7) sin(r7) 0;
-            ]; %
-%         quad_body = cat(3,[
-%                    % body                      
-%                       1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16] , [
-%                       13 14 15 16; 17 18 19 20;21 22 23 24;25 26 27 28]);
+       dip1 = 4; dip2 =1; h1 = 5.5; h2 = 4.125; h3 = 2.75; h4 =1.375; h5 = 0; d = 0.85; r = 3.343; rn1 = 1*pi/96; r0 = 2*pi/96; r1 = 3*pi/96; r2 = 9*pi/96; r3 = 15*pi/96; r4 = 21*pi/98; r5 = 22*pi/96; r6 = 23*pi/96; r7 = 24*pi/96; i = 1/2; verts = [ 0 0 dip1; % Start
+            0 0 dip1;
+            0 0 dip1;
+            0 0 dip1; % End
+            d*cos(r1) d*sin(r1) h1; % Start
+            cos(r2) sin(r2) h1;
+            cos(r3) sin(r3) h1;
+            d*cos(r4) d*sin(r4) h1; % End
+            d*r*cos(r1) d*r*sin(r1) h2; % Start
+            r*cos(r2) r*sin(r2) h2;
+            r*cos(r3) r*sin(r3) h2;
+            d*r*cos(r4) d*r*sin(r4) h2; % End
+            d*r*cos(r1) d*r*sin(r1) h3; % Start
+            r*cos(r2) r*sin(r2) h3;
+            r*cos(r3) r*sin(r3) h3;
+            d*r*cos(r4) d*r*sin(r4) h3; % End
+            d*r*cos(r1) d*r*sin(r1) h4; % Start
+            r*cos(r2) r*sin(r2) h4;
+            r*cos(r3) r*sin(r3) h4;
+            d*r*cos(r4) d*r*sin(r4) h4; % End
+            d*cos(r1) d*sin(r1) h5; % Start
+            cos(r2) sin(r2) h5;
+            cos(r3) sin(r3) h5;
+            d*cos(r4) d*sin(r4) h5; % 
+            0 0 dip2;
+            0 0 dip2;
+            0 0 dip2;
+            0 0 dip2; %
+            d*cos(r0) d*sin(r0) h1; %%%%%%%%%%%%%% 
+            cos(rn1) sin(rn1) h1;
+            cos(0) sin(0) h1;  % 1
+            d*r*cos(r0) d*r*sin(r0) h2;
+            r*cos(rn1) r*sin(rn1) h2;
+            r*cos(0) r*sin(0) h2; % 2
+            d*r*cos(r0) d*r*sin(r0) h3;
+            r*cos(rn1) r*sin(rn1) h3;
+            r*cos(0) r*sin(0) h3; % 3 
+            d*r*cos(r0) d*r*sin(r0) h4;
+            r*cos(rn1) r*sin(rn1) h4;
+            r*cos(0) r*sin(0) h4; % 4
+            d*cos(r0) d*sin(r0) h5;
+            cos(rn1) sin(rn1) h5;
+            cos(0) sin(0) h5; %%%%%%%%%%
+            d*cos(r5) d*sin(r5) h1;
+            cos(r6) sin(r6) h1;
+            cos(r7) sin(r7) h1;  % 1
+            d*r*cos(r5) d*r*sin(r5) h2;
+            r*cos(r6) r*sin(r6) h2;
+            r*cos(r7) r*sin(r7) h2; % 2
+            d*r*cos(r5) d*r*sin(r5) h3;
+            r*cos(r6) r*sin(r6) h3;
+            r*cos(r7) r*sin(r7) h3; % 3 
+            d*r*cos(r5) d*r*sin(r5) h4;
+            r*cos(r6) r*sin(r6) h4;
+            r*cos(r7) r*sin(r7) h4; % 4
+            d*cos(r5) d*sin(r5) h5;
+            cos(r6) sin(r6) h5;
+            cos(r7) sin(r7) h5;
+            ]; 
